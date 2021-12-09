7f06bb97c854e745e3c64ad020e5d39f053ff378
Mateusz Borowiecki <borowieckimateusz1@gmail.com>
2021-12-09 19:40:06 +0100

    update/cards_database 	changed: Second english cards set to entirely new set 	added: First polish cards set

--- backend/data/cardSets.ts
+++ backend/data/cardSets.ts
@@ -46,48 +46,347 @@ export default {
             ]
         },
         {
-            "name": "Set #2",
+            "name": "Jobs",
             "cards": [
                 {
-                    "word": "Snake",
+                    "word": "Policeman",
                     "wordsNotToUse": [
-                        "Slither",
-                        "Cobra",
-                        "Serpent",
-                        "Sly",
-                        "Grass"
+                        "Violence",
+                        "Arrest",
+                        "Punish",
+                        "Police car"
                     ]
                 },
                 {
-                    "word": "Car",
+                    "word": "Waiter/Waitress",
                     "wordsNotToUse": [
-                        "Driver",
-                        "Ride",
-                        "Transport",
-                        "Fast",
-                        "Travel"
+                        "Meal",
+                        "Restaurant",
+                        "Plate",
+                        "Cutlery",
                     ]
                 },
                 {
-                    "word": "Dragonfly",
+                    "word": "Dustman/Garbage man",
                     "wordsNotToUse": [
-                        "Red",
-                        "Wings",
-                        "Insect",
-                        "Fly",
-                        "Dragon"
+                        "Garbage",
+                        "Container",
+                        "Dustcart",
+                        "Overalls"
                     ]
                 },
                 {
-                    "word": "Snowflake",
+                    "word": "Receptionist",
                     "wordsNotToUse": [
-                        "Cold",
-                        "Winter",
+                        "Greet",
+                        "Telephone",
+                        "Visitors",
+                        "Reception desk"
+                    ]
+                },
+                {
+                    "word": "Gardener",
+                    "wordsNotToUse": [
+                        "Lawn",
+                        "Mow",
+                        "Garden(ing)",
+                        "Water"
+                    ]
+                },
+                {
+                    "word": "Hunter",
+                    "wordsNotToUse": [
+                        "Hunt",
+                        "Rifle",
+                        "Animal(s)",
+                        "Danger(ous)"
+                    ]
+                },
+                {
+                    "word": "Pilot",
+                    "wordsNotToUse": [
+                        "Plane",
+                        "Sky",
+                        "Stewardess",
+                        "Fly"
+                    ]
+                },
+                {
+                    "word": "Professor",
+                    "wordsNotToUse": [
+                        "University",
+                        "Academic",
+                        "Speech",
+                        "Teach"
+                    ]
+                },
+                {
+                    "word": "Salesman/Saleswoman",
+                    "wordsNotToUse": [
+                        "Shop",
+                        "Buy",
+                        "Customer",
+                        "Pay"
+                    ]
+                },
+                {
+                    "word": "Politician",
+                    "wordsNotToUse": [
+                        "Politics",
+                        "State",
+                        "Government",
+                        "Political party"
+                    ]
+                },
+                {
+                    "word": "Construction Worker",
+                    "wordsNotToUse": [
+                        "Building",
+                        "Construct",
+                        "Wall",
+                        "Helmet"
+                    ]
+                },
+                {
+                    "word": "Lawyer",
+                    "wordsNotToUse": [
+                        "Law",
+                        "Court",
+                        "Crime",
+                        "Advise"
+                    ]
+                },
+                {
+                    "word": "Florist",
+                    "wordsNotToUse": [
                         "Flower",
-                        "Snow",
-                        "Fall"
+                        "Sell",
+                        "Flower shop",
+                        "Customer"
+                    ]
+                },
+                {
+                    "word": "Actor/Actress",
+                    "wordsNotToUse": [
+                        "Perform",
+                        "Film",
+                        "Stage",
+                        "Act"
+                    ]
+                },
+                {
+                    "word": "Hairdresser",
+                    "wordsNotToUse": [
+                        "Cut",
+                        "Scissors",
+                        "Hair",
+                        "Wash"
+                    ]
+                },
+                {
+                    "word": "Firefighter",
+                    "wordsNotToUse": [
+                        "Water",
+                        "Fire",
+                        "Helmet",
+                        "Fire engine"
+                    ]
+                },
+                {
+                    "word": "Musician",
+                    "wordsNotToUse": [
+                        "Musical instrument",
+                        "Play",
+                        "Audience",
+                        "Music"
+                    ]
+                },
+                {
+                    "word": "Postman",
+                    "wordsNotToUse": [
+                        "Letters",
+                        "Mail",
+                        "Postbox",
+                        "Newspaper"
                     ]
                 }
+            ]
+        }
+    ],
+    "pl": [
+        {
+            "name": "Wesele",
+            "cards": [
+                {
+                    "word": "Pierwszy taniec",
+                    "wordsNotToUse": [
+                        "Muzyka",
+                        "Pierwszy raz",
+                        "Tańczyć",
+                        "Sami",
+                        "Melodia"
+                    ]
+                },
+                {
+                    "word": "Kościół",
+                    "wordsNotToUse": [
+                        "Ksiądz",
+                        "Msza",
+                        "Ofiara",
+                        "Ślub",
+                        "Ceremonia"
+                    ]
+                },
+                {
+                    "word": "Orkiestra",
+                    "wordsNotToUse": [
+                        "Muzyka",
+                        "Tańce",
+                        "Grać",
+                        "Instrument",
+                        "Pierwszy taniec"
+                    ]
+                },
+                {
+                    "word": "Panna młoda",
+                    "wordsNotToUse": [
+                        "Sukienka",
+                        "Biały",
+                        "Kobieta",
+                        "Dziewica",
+                        "Welon"
+                    ]
+                },
+                {
+                    "word": "Świadkowa",
+                    "wordsNotToUse": [
+                        "Świadek",
+                        "Panna Młoda",
+                        "Kobieta",
+                        "Bukiet",
+                        "Suknia ślubna"
+                    ]
+                },
+                {
+                    "word": "Tort weselny",
+                    "wordsNotToUse": [
+                        "Ślub",
+                        "Tort",
+                        "Ciasto",
+                        "Słodkie",
+                        "Jeść"
+                    ]
+                },
+                {
+                    "word": "Szczęście",
+                    "wordsNotToUse": [
+                        "Przysięga",
+                        "Uśmiech",
+                        "Pan Młody",
+                        "Panna Młoda",
+                        "Piosenka"
+                    ]
+                },
+                {
+                    "word": "Teść",
+                    "wordsNotToUse": [
+                        "Ojciec",
+                        "Panna Młoda",
+                        "Pan Młody",
+                        "Wychować",
+                        "Rodzina"
+                    ]
+                },
+                {
+                    "word": "Butonierka",
+                    "wordsNotToUse": [
+                        "Kwiaty",
+                        "Garnitur",
+                        "Pan Młody",
+                        "Ozdoba",
+                        "Tradycja"
+                    ]
+                },
+                {
+                    "word": "Oczepiny",
+                    "wordsNotToUse": [
+                        "Bukiet",
+                        "Welon",
+                        "Muszka",
+                        "Rzucać",
+                        "Północ"
+                    ]
+                },
+                {
+                    "word": "Miłość",
+                    "wordsNotToUse": [
+                        "Romantyzm",
+                        "Wesele",
+                        "Ślub",
+                        "Pocałunek",
+                        "Młodzi"
+                    ]
+                },
+                {
+                    "word": "Wesele",
+                    "wordsNotToUse": [
+                        "Goście",
+                        "Pan Młody",
+                        "Panna Młoda",
+                        "Wódka",
+                        "Tańce"
+                    ]
+                },
+                {
+                    "word": "Pan Młody",
+                    "wordsNotToUse": [
+                        "Narzeczona",
+                        "Mężczyzna",
+                        "Garnitur",
+                        "Butonierka",
+                        "Kawalerski"
+                    ]
+                },
+                {
+                    "word": "Świadek",
+                    "wordsNotToUse": [
+                        "Przyjaciel",
+                        "Pan Młody",
+                        "Przemowa",
+                        "Druhna",
+                        "Świadczyć"
+                    ]
+                },
+                {
+                    "word": "Sala",
+                    "wordsNotToUse": [
+                        "Wynajem",
+                        "Pomieszczenie",
+                        "Parkiet",
+                        "Wesele",
+                        "Dekoracje"
+                    ]
+                },
+                {
+                    "word": "Pierścionek",
+                    "wordsNotToUse": [
+                        "Krążek",
+                        "Złoto",
+                        "Palec",
+                        "Zaręczyny",
+                        "Wydatek"
+                    ]
+                },
+                {
+                    "word": "Wódka",
+                    "wordsNotToUse": [
+                        "Alkohol",
+                        "Kac",
+                        "Polewać",
+                        "Zimna",
+                        "Czysta"
+                    ]
+                },
             ]
         }
     ]
